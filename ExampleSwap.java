2bdecdbf5dfa4d80016cb5c13e90f5fba0aed966
Avadhut Mokate <mokateavadhut@gmail.com>
2025-08-29 19:28:02 +0530

    Add files via upload

---
+++ ExampleSwap.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class ExampleSwap
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        int b = 20;
+        System.out.println("Before Swap");
+        System.out.println("a= "+a);
+        System.out.println("b= "+b);
+        
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After Swap");
+        System.out.println("a : "+a);
+        System.out.println("b : "+b);
+        
+    }
+}
